44ba7acb2c57dd2ef4d067eefa9312421d35eb60
alejandrodh <alejandro@digitalhouse.com>
2019-06-29 09:40:44 -0300

    Agrego relaciones entre tablas (FK).

--- app/Product.php
+++ app/Product.php
@@ -9,8 +9,10 @@ class Product extends Model
 
     // public $table = "Products";
     public $guarded = [];
-
-
     // public $timestamps = false;
+
+    public function users(){
+      return $this->belongsTo('\App\User', 'user_id');
+    }
 
 }
